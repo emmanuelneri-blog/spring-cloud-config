878c34a398d5891ce6a632955912a182ad82b699
Emmanuel Neri <emmanuelnerisouza@gmail.com>
2018-05-02 20:34:30 -0300

    Testando retry no spring cloud  config

--- app/src/main/java/br/com/emmanuelneri/app/AppConfig.java
+++ app/src/main/java/br/com/emmanuelneri/app/AppConfig.java
@@ -2,8 +2,10 @@ package br.com.emmanuelneri.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableRetry
 public class AppConfig {
 
     public static void main(String[] args) {
