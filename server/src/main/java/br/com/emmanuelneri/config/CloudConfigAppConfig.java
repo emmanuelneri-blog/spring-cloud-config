f893361ab480575b7230eb62a44e901c4ff107a9
Emmanuel Neri <emmanuelnerisouza@gmail.com>
2018-05-02 22:36:31 -0300

    Configurando spring security no config server

--- server/src/main/java/br/com/emmanuelneri/config/CloudConfigAppConfig.java
+++ server/src/main/java/br/com/emmanuelneri/config/CloudConfigAppConfig.java
@@ -1,14 +1,41 @@
 package br.com.emmanuelneri.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableConfigServer
-public class CloudConfigAppConfig {
+public class CloudConfigAppConfig extends WebSecurityConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(CloudConfigAppConfig.class, args);
+    }
+
+    @Value("${security.user.name}")
+    private String user;
+
+    @Value("${security.user.password}")
+    private String password;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser(user)
+                .password(password)
+                .roles("ADMIN");
     }
 }
