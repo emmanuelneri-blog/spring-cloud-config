9073cf4e84225559435c3541b3eca7c923cf217e
Emmanuel Neri <emmanuelnerisouza@gmail.com>
2018-11-22 21:42:43 -0200

    Ajustando configuracao do spring security

--- server/src/main/java/br/com/emmanuelneri/config/CloudConfigAppConfig.java
+++ server/src/main/java/br/com/emmanuelneri/config/CloudConfigAppConfig.java
@@ -24,7 +24,8 @@ public class CloudConfigAppConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.csrf().disable()
+                .authorizeRequests()
                 .anyRequest()
                 .authenticated()
                 .and()
